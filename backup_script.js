15044492e2cc3e5e07713c0f6b2977404d579ad6
nabilabakery122 <nabillabakery122@gmail.com>
2021-04-09 16:54:25 +0700

    teratest_v2_1_i

---
+++ backup_script.js
@@ -0,0 +1,42 @@
+<script type="text/javascript">
+		// $(document).ready(function(){
+		// 	$('#form2').hide()
+		// 	$('#form1').on('click', '#submit1', function(e){
+		// 		// e.preventDefault()
+		// 		$('#form1').hide('slow')
+		// 	})
+		// 		$('#form2').show('slow')
+		// })
+		
+		// $(document).ready(function(){
+		// 	const object = {
+		// 		form1: {
+		// 			id: $('#form1'),
+		// 			baris: $('#baris').val(),
+		// 			kolom: $('#kolom').val(),
+		// 			submit: $('#submit1')
+		// 		},
+		// 		form2: {
+		// 			id: $('#form2')
+		// 		}
+		// 	}
+
+		// 	// object.form2.id.hide('slow')
+
+		// 	object.form1.submit.on('click', function(e){
+		// 		e.preventDefault()
+		// 		console.log(object.form1.baris)
+		// 		console.log(object.form1.kolom)
+		// 		if(object.form1.baris === null || object.form1.kolom === null){
+		// 			$('#result').html(`
+		// 				<h1>Harap isi input terlebih dahulu</h1>
+		// 				`)
+		// 		}else{
+		// 			// object.form1.id.hide('slow')
+		// 			// object.form2.id.show('slow')
+		// 		}
+		// 	})
+		// })
+
+
+	</script>
---
+++ soal1.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Soal-1</title>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<fieldset>
+	<?php if(!isset($_POST['submit2'])): ?>
+		<div id="form1">			
+			<form action="" method="post">
+				<ul>
+					<li>				
+						<label for="baris">Input jumlah baris</label>
+						<input type="number" id="baris" name="baris">
+					</li>
+					<li>
+						<label for="kolom">Input jumlah kolom</label>
+						<input type="number" name="kolom" id="kolom">
+					</li>
+					<li>
+						<button type="submit" name="submit1">Submit</button>
+					</li>
+				</ul>
+			</form>
+		</div>
+	<?php endif; ?>
+
+		<div id="form2">
+			<form action="" method="post">
+				<ul>
+					<?php  
+					if(isset($_POST['submit1'])):						
+						$baris = $_POST['baris'];
+						$kolom = $_POST['kolom'];
+					?>
+						<table>
+						<?php for($i=1; $i <=$baris; $i++):?>
+							<tr>
+							<?php for($j=1; $j<=$kolom; $j++):?>
+								<td>
+								<label><?=$i.'.'.$j?>: </label>
+								<input type='text' name='input[]'>
+								<?php // echo $i.$j; ?>
+								</td>
+							<?php endfor;?>
+						<?php endfor;?>
+
+						</table>
+						<button type='submit' name='submit2'>Submit</button>
+					<?php endif;?>
+				</ul>
+			</form>
+		</div>
+		
+
+		<div id="result">
+			<?php  if(isset($_POST['submit2'])):?>
+				<fieldset>
+					<legend>Result Soal-1</legend>
+					<?php 
+						$input = $_POST['input'];
+						for($i = 1; $i < count($input); $i++){
+							// echo $i.': ';
+							echo $input[$i].'.'.$j;
+							for($j = 1; $j < $i; $j++){
+							}
+						}
+					?>
+				</fieldset>
+			<?php endif;?>
+
+		</div>
+
+	</fieldset>
+
+
+	<style type="text/css">
+		ul li {
+			list-style: none;
+		}
+		input {
+			margin-bottom: 2rem;
+			margin-left: .7rem;
+		}
+		#form1{
+			margin-bottom: 5rem;
+		}
+
+		#form2{
+			display: flex;
+			justify-content: center;
+		}
+
+		#result{
+			margin-top: 7rem;
+		}
+	</style>
+
+
+	
+</body>
+</html>
